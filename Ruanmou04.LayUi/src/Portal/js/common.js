e5f312ff532bf55fea054d22c097a96794cb7ab5
EighthNight <54519304+EighthNight@users.noreply.github.com>
2019-08-29 18:41:53 +0800

    客户端HTML首次提交
    layui.js 增加config.js的模块配置
    增加客户端 Portal 文件

---
+++ Ruanmou04.LayUi/src/Portal/js/common.js
@@ -0,0 +1,12 @@
+﻿//全局js
+var common = {
+    Login: function ($, layer) {
+        $(".a-login").click(function () {
+            layer.open({
+                title: '在线调试'
+                , content: '可以填写任意的layer代码'
+            });  
+        })
+        console.log('这是common');
+    }
+};
