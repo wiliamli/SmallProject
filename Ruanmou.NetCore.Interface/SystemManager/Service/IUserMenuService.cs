d3b8a82199ed3af996520dfc04a259fd850a6ca0
lisi <lisi@HUANGBANGSONG>
2019-09-09 22:55:16 +0800

    jwt

---
+++ Ruanmou.NetCore.Interface/SystemManager/Service/IUserMenuService.cs
@@ -0,0 +1,12 @@
+﻿using RM04.DBEntity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ruanmou04.NetCore.Interface.SystemManager.Service
+{
+    public interface IUserMenuService
+    {
+        List<SysMenuTreeDto> GetAuthorityMenuList(int userID);
+    }
+}
---
+++ Ruanmou.NetCore3_0.DemoProject/Models/CurrentUserInfo.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using RM04.DBEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Ruanmou04.NetCore.Project.Models
+{
+    public class CurrentUserInfo: ICurrentUserInfo
+    {
+        IHttpContextAccessor contextAccessor;
+        public CurrentUserInfo(IHttpContextAccessor contextAccessor)
+        {
+            this.contextAccessor = contextAccessor;
+        }
+        public CurrentUser CurrentUser
+        {
+            get
+            {
+                var identity = contextAccessor.HttpContext.User;
+                var users = new CurrentUser();
+                if (identity.FindFirst(ClaimTypes.PrimarySid) != null)
+                {
+                    users.Id =Convert.ToInt32( identity.FindFirst(ClaimTypes.PrimarySid).Value);
+                    users.Name = identity.FindFirst(ClaimTypes.Name).Value;
+                }
+                return users;
+            }
+        }
+    }
+}
