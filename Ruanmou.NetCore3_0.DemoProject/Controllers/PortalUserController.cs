50c6c05084a4f3ded4676d587718b17260c04abe
EighthNight <54519304+EighthNight@users.noreply.github.com>
2019-09-11 12:04:59 +0800

    会员登陆获取会员信息

---
+++ Ruanmou.NetCore3_0.DemoProject/Controllers/PortalUserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using RM04.DBEntity;
+using Ruanmou04.NetCore.Project.Controllers;
+using Ruanmou04.NetCore.Project.Models;
+using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace Ruanmou.NetCore3_0.DemoProject.Controllers
+{
+
+    [Route("api/[controller]/[action]")]
+    [ServiceFilter(typeof(VerifyAttribute))]
+    [ApiController]
+    public class PortalUserController : BaseApiController
+    {
+        public PortalUserController(IMemoryCache memoryCache) : base(memoryCache)
+        {
+
+        }
+
+        [HttpGet()]
+        public SysUserOutputDto GetUser()
+        {
+            SysUserOutputDto sysUser = base.GetUserInfo();
+            return sysUser;
+        }
+
+         
+
+    }
+}
