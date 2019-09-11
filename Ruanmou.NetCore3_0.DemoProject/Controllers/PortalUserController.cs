3ea2cbe3994cc7d12d6cff2335876563ffb4b9e1
EighthNight <54519304+EighthNight@users.noreply.github.com>
2019-09-11 15:20:23 +0800

    获取用户

--- Ruanmou.NetCore3_0.DemoProject/Controllers/PortalUserController.cs
+++ Ruanmou.NetCore3_0.DemoProject/Controllers/PortalUserController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using RM04.DBEntity;
+using Ruanmou.NetCore.Interface;
+using Ruanmou04.EFCore.Model.DtoHelper;
 using Ruanmou04.NetCore.Project.Controllers;
 using Ruanmou04.NetCore.Project.Models;
+using Ruanmou04.NetCore.Service.Authorization;
+using System.Threading.Tasks;
 using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
 using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
@@ -14,19 +18,67 @@ namespace Ruanmou.NetCore3_0.DemoProject.Controllers
     [ApiController]
     public class PortalUserController : BaseApiController
     {
-        public PortalUserController(IMemoryCache memoryCache) : base(memoryCache)
-        {
+        private ISysUserService _IUserService = null;
+        private IMemoryCache _memoryCache = null;
+        private ICurrentUserInfo _CurrentUser = null;
 
+        public PortalUserController(ISysUserService userService,
+            ICurrentUserInfo currentUser,
+            IMemoryCache memoryCache) : base(memoryCache)
+        {
+            this._IUserService = userService;
+            this._memoryCache = memoryCache;
+            this._CurrentUser = currentUser;
         }
 
-        [HttpGet()]
+        [HttpGet]
         public SysUserOutputDto GetUser()
         {
             SysUserOutputDto sysUser = base.GetUserInfo();
+            var user = _CurrentUser.CurrentUser;
             return sysUser;
         }
 
-         
+        [HttpPost]
+        public AjaxResult SaveUser([FromBody]SysUserOutputDto model)
+        { 
+            SysUser user = _IUserService.Find<SysUser>(model.Id);
+           
+            user.Name = model.Name;
+            user.Mobile = model.Mobile;
+            user.QQ = model.QQ;
+            user.WeChat = model.WeChat;
+            if(string.IsNullOrWhiteSpace(model.Sex)) user.Sex = model.Sex; 
+
+            _IUserService.Update(user);
+
+           // this._memoryCache.Set<SysUserOutputDto>(ajax.data, sysuserdto);
+
+            return new AjaxResult()
+            {
+                success = true,
+                msg = "ok",
+            };
+        }
+
+        [HttpPost]
+        public AjaxResult SaveUserV2(SysUserOutputDto model)
+        {
+            SysUser user = _IUserService.Find<SysUser>(model.Id);
+
+            //user.Name = model.Name;
+            //user.Mobile = model.Mobile;
+            //user.QQ = model.QQ;
+            //user.WeChat = model.WeChat;
+            //if (string.IsNullOrWhiteSpace(model.Sex)) user.Sex = model.Sex;
+
+            //_IUserService.Update(user);
+            return new AjaxResult()
+            {
+                success = true,
+                msg = "ok",
+            };
+        }
 
     }
 }
