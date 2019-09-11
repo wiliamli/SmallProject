111e5681db057213be83312b4f1370dd7d1a0b9d
EighthNight <54519304+EighthNight@users.noreply.github.com>
2019-09-11 15:25:46 +0800

    config修改

--- Ruanmou04.LayUi/lay/modules/config.js
+++ Ruanmou04.LayUi/lay/modules/config.js
@@ -2,15 +2,6 @@
  * 配置文件 
  * ***/
 
-layui.define(function (exports) { 
-    var config = {
-        apiUrl: 'https://localhost:44367/', 
-        Verson: '1.0.0.1',
-    } 
-    //操作当前实例 
-    exports('config', config);
-});
-
 /**
 
  @Title: layui.config
@@ -23,8 +14,8 @@ layui.define('layer', function (exports) {
  
     var MOD_NAME = 'config'
      , config = {
-         apiUrl: 'https://localhost:44367/api',
-         fileUrl: "https://localhost:44367/fileUpload/"
+         apiUrl: 'https://localhost:5001/api',
+         fileUrl: "https://localhost:5001/fileUpload/"
      }
 
  //操作当前实例 
