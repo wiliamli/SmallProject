e04a60ee12ed6baee03df36d6251dcce7d598df6
wangshanshan <wangshanshan@lvmama.com>
2019-09-26 15:46:24 +0800

    修改登录使用Account字段来匹配！

--- Ruanmou.NetCore.Service/SystemManager/LoginService.cs
+++ Ruanmou.NetCore.Service/SystemManager/LoginService.cs
@@ -17,26 +17,19 @@ namespace Ruanmou.NetCore.Service
         }
         public AjaxResult Login(LoginInputDto loginInput)
         {
-            AjaxResult ajaxResult = new AjaxResult() { success = false };
-
-            var user = base.Find<SysUser>(u => u.Name == loginInput.Account);
-            if (user == null)
+            var user = base.Find<SysUser>(u => u.Account == loginInput.Account);
+            if (user == null || user.Id <= 0)
             {
-                ajaxResult.msg = "用户名或密码不正确,请检查！";
+                return AjaxResult.Failure("用户名或密码不正确,请检查！");
             }
             var password = Encrypt.EncryptionPassword(loginInput.Password);
             if (user.Password != password)
             {
-                ajaxResult.msg = "用户名或密码不正确,请检查！";
+                return AjaxResult.Failure("用户名或密码不正确,请检查！");
             }
 
-            else//MapTo
-            {
-                ajaxResult.data = user.MapTo<SysUser, SysUserOutputDto>();// DataMapping<SysUser, SysUserOutputDto>.Trans(user);
-                ajaxResult.success = true;
-                ajaxResult.msg = "登录成功";
-            }
-            return ajaxResult;
+            var data = DataMapping<SysUser, SysUserOutputDto>.Trans(user);
+            return AjaxResult.Success("登录成功", data);
         }
     }
 }
--- Ruanmou04.NetCore.Dtos/DtoHelper/AjaxResult.cs
+++ Ruanmou04.NetCore.Dtos/DtoHelper/AjaxResult.cs
@@ -7,21 +7,6 @@ namespace Ruanmou04.EFCore.Dtos.DtoHelper
     /// </summary>
     public class AjaxResult
     {
-        /// <summary>
-        /// 是否成功
-        /// </summary>
-        public bool success { get; set; }
-
-        /// <summary>
-        /// 数据
-        /// </summary>
-        public object data { get; set; }
-
-        /// <summary>
-        /// 返回消息
-        /// </summary>
-        public string msg { get; set; }
-
         public AjaxResult(string errorMsg)
         {
             //this.success = false;
@@ -38,16 +23,47 @@ namespace Ruanmou04.EFCore.Dtos.DtoHelper
         {
             data = result;
         }
-
-        public AjaxResult()
-        {
-
-        }
-
         public AjaxResult(string errorMsg, bool isSuccess)
         {
             this.success = isSuccess;
             this.msg = errorMsg;
+        }
+        public AjaxResult()
+        {   
+        }
+
+        /// <summary>
+        /// 是否成功
+        /// </summary>
+        public bool success { get; set; }
+
+        /// <summary>
+        /// 数据
+        /// </summary>
+        public object data { get; set; }
+
+        /// <summary>
+        /// 返回消息
+        /// </summary>
+        public string msg { get; set; }
+         
+        public static AjaxResult Success(string msg,object data)
+        {
+            return new AjaxResult()
+            {
+                success = true,  
+                msg = msg,
+                data=data
+            };
+        }
+
+        public static AjaxResult Failure(string msg)
+        {
+            return new AjaxResult()
+            {
+                success = false,   
+                msg = msg,
+            };
         }
     }
 }
