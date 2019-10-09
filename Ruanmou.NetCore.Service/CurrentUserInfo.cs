6bb479426d4037d04133156896ba200e73b5fce9
lijunliang <l_jl42@aliyun.com>
2019-10-09 17:33:40 +0800

    修改判断token

--- Ruanmou.NetCore.Service/CurrentUserInfo.cs
+++ Ruanmou.NetCore.Service/CurrentUserInfo.cs
@@ -57,25 +57,28 @@ namespace Ruanmou04.NetCore.Service
         private CurrentUser GetCurrentUser(string headerKey)
         {
             var identity = contextAccessor.HttpContext.User;
-            var users = new CurrentUser();
-            //if (identity.FindFirst(ClaimTypes.PrimarySid) == null)
-            //{
+            CurrentUser users = null; 
+            
             if (contextAccessor.HttpContext.Request.Headers.ContainsKey(headerKey))
             {
-                contextAccessor.HttpContext.Request.Headers.TryGetValue(headerKey, out StringValues val);
-                users.Id = Convert.ToInt32(identity.FindFirst(ClaimTypes.PrimarySid).Value);
-                users.Name = identity.FindFirst(ClaimTypes.Name).Value;
-                identity = tokenService.GetClaims(val);
-                var curRoles = GetRoleCache(val);
-                if (curRoles != null && curRoles.Count() > 0)
+                if (identity.FindFirst(ClaimTypes.PrimarySid) != null)
                 {
-                    users.SysRoles = curRoles;
-                }
-                else
-                {
-                    curRoles = this.sysRoleApplication.GetUserRoles(users.Id);
-                    System.Threading.Tasks.Task.Run(() => SetRoleCache(val, curRoles));
-                    users.SysRoles = curRoles;
+                    users = new CurrentUser();
+                    contextAccessor.HttpContext.Request.Headers.TryGetValue(headerKey, out StringValues val);
+                    users.Id = Convert.ToInt32(identity.FindFirst(ClaimTypes.PrimarySid).Value);
+                    users.Name = identity.FindFirst(ClaimTypes.Name).Value;
+                    identity = tokenService.GetClaims(val);
+                    var curRoles = GetRoleCache(val);
+                    if (curRoles != null && curRoles.Count() > 0)
+                    {
+                        users.SysRoles = curRoles;
+                    }
+                    else
+                    {
+                        curRoles = this.sysRoleApplication.GetUserRoles(users.Id);
+                        System.Threading.Tasks.Task.Run(() => SetRoleCache(val, curRoles));
+                        users.SysRoles = curRoles;
+                    }
                 }
             }
             return users;
